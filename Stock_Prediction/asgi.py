dc9a7c65b20de3c2af2ecf10dbc577f96fb87855
Sanket-Kumbhare <sanketkumbhare1511@gmail.com>
2021-03-21 03:41:36 +0530

    initial commit

---
+++ Stock_Prediction/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Stock_Prediction project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stock_Prediction.settings')
+
+application = get_asgi_application()
